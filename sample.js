bbd9f0b3c48547dd8d1185cd5bcf0c78571b901e
chandanayalla <chandanayalla2006@gmail.com>
2024-08-02 19:37:02 +0530

    ok

--- sample.js
+++ sample.js
@@ -1,8 +1,8 @@
 document.getElementById("chandana").innerHTML="<h1>chandana</h1>"
 
-document.getElementsByClassName("chandana")[0].textContent="<h1>chandana</h1>"
+document.getElementsByClassName("chan")[0].textContent="<h1>chandana</h1>"
 
-document.getElementsByTagName
-document.querySelector("chandana")
-document.querySelectorAll(".chandana")
+// document.getElementsByTagName
+// document.querySelector("chandana")
+// document.querySelectorAll(".chandana")
 
